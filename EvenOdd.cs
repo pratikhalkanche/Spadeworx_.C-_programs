ae8936e01bc89c0c7a86338d959e381d2c310a0a
pratikhalkanche <78910594+pratikhalkanche@users.noreply.github.com>
2021-07-19 12:05:54 +0530

    Add files via upload

---
+++ EvenOdd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace firstproject
+{
+    class EvenOdd
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("It is even number");
+            }
+
+        }
+    }
+
+
+}
+
---
+++ Exception.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+                                   //Multiple Catch inprograms
+namespace firstproject
+{
+    class Exception
+    {
+            public void show()
+            {
+                int a = 10, b = 5, c;
+                try
+                {
+
+                    c = a / b;
+                    try
+                    {
+
+
+                        int[] arr = new int[3];
+                        arr[3] = 10;
+                        int[] arr1 = null;
+
+                        foreach (int i in arr1)
+                        {
+                            Console.WriteLine(arr[i]);
+                        }
+                    }
+                    catch (IndexOutOfRangeException io)
+                    {
+                        Console.WriteLine(io.Message);
+                    }
+
+                }
+                catch (DivideByZeroException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                finally
+                {
+                    Console.WriteLine("finally is executed");
+                }
+
+            }
+        }
+        class ExceptionEx
+        {
+            static void Main(string[] args)
+            {
+            Exception e2 = new Exception();
+                e2.show();
+            }
+        }
+    }
+
